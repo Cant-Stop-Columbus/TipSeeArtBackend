b7d0bd7f397a6b18455d1a3ba68eb925622cc2fb
Collin M. McCabe <collinmichaelmccabe@gmail.com>
2023-04-30 12:13:54 -0400

    Adds reminder to input new models to the init

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,3 +1,8 @@
+#! NB: After adding new Model classes, make sure to add them here so they can be accessible to other modules!
+#!     If adding a new class to an existing file in this folder, use the following syntax:
+#!       `from .{existing_filename_without_dot_py} import ExistingClassName, {NewClassName}`
+#!     And if you've just created a new file containing at least one new class to import:
+#!       `from .{new_filename_without_dot_py} import {NewClassName}`
 from .user import User
 from .artist import Artist
 from .payment_providers import PaymentProider
