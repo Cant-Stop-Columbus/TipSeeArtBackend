d2678f2d7d274b96641a7589a72218feac7cf14f
Collin M. McCabe <collinmichaelmccabe@gmail.com>
2023-04-30 12:21:02 -0400

    Adds reminder to input new Controllers to init

--- api/controllers/__init__.py
+++ api/controllers/__init__.py
@@ -1,3 +1,9 @@
+#! NB: After adding new Controller classes, make sure to add them here so they can be accessible to other modules!
+#!     If adding a new class to an existing file in this folder, use the following syntax WITH AN EXISTING LINE:
+#!       `from .{existing_filename_without_dot_py} import ExistingClassName, {NewClassName}`
+#!     And if you've just created a new file containing at least one new class to import, CREATE A NEW LINE AS SO:
+#!       `from .{new_filename_without_dot_py} import {NewClassName}`
 from .default import *
 from .artists import *
 from .users import *
+from .media import *
